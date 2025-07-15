8490009fc7eb192a0e750907be99e15b7f774f58
praneeth chanti <praneeth2216@hotmail.com>
2025-07-15 23:20:47 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Praneeth</title>
+</head>
+<body>
+    <h1>
+        <?php
+            $name = "Praneeth";
+            echo "Hello, " . $name . "!";
+        ?>
+    </h1>
